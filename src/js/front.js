ffdbdde86a1efbc635e31e2b5a19f1ceb3c14a6a
marlass <lasakmarcin90@gmail.com>
2017-01-15 09:40:35 +0100

    Add photoswipe

--- src/js/front.js
+++ src/js/front.js
@@ -178,5 +178,26 @@ if (area && garages && projects && build && garden && terrace) {
     terrace.addEventListener('change', function(e) {
         calculatePrice();
     });
-
 }
+
+var pswpElement = document.querySelectorAll('.pswp')[0];
+
+var items = [
+    {
+        src: 'https://placekitten.com/600/400',
+        w: 600,
+        h: 400
+    },
+    {
+        src: 'https://placekitten.com/1200/900',
+        w: 1200,
+        h: 900
+    }
+];
+
+var options = {
+    index: 0
+};
+
+var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+gallery.init();
