47735d8853da5559ae735bc95ad3441c1f240a22
Marcin Lasak <lasakmarcin90@gmail.com>
2017-01-16 15:58:20 +0100

    Fix bugs

--- src/js/front.js
+++ src/js/front.js
@@ -108,7 +108,7 @@ function calculatePrice () {
         if (build) {
             resultText += ` with build cost estimation`;
         }        
-        resultText += ` from ${floor(price/5)}£.`;
+        resultText += ` from ${Math.floor(price/5)}£.`;
     } else {
         if (garden && terrace) {
             resultText = `Oferujemy projekt domu jednorodzinnego o metrażu ${area}m<sup>2</sup> z ogrodem i tarasem`;
@@ -132,7 +132,7 @@ function calculatePrice () {
         if (build) {
             resultText += ` z wyceną budowy`;
         }        
-        resultText += ` od ${floor(price)}zł.`;
+        resultText += ` od ${Math.floor(price)}zł.`;
     }
 
     result.innerHTML = resultText;
